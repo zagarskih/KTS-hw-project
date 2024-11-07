7a9c8303fe58bb49d2b108614871bc3b9b5e62ee
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2024-11-07 15:14:54 +0400

    refactor styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom/client';
 import App from './App';
-import './index.scss';
+import 'styles/index.scss';
 
 const rootElement = document.getElementById('root');
 
