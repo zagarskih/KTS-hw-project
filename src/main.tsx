28cdf6a966518e66428a118fff9286b2ba2a5991
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2024-11-07 14:39:04 +0400

    upd styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom/client';
 import App from './App';
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 
